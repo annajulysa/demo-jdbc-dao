1654afed79110beffd38f30f91ffc3a6cec699a8
Anna Almada <annasalmada@gmail.com>
2024-09-18 22:32:15 +0100

    aux methods FindById

--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -55,17 +55,8 @@ public class SellerDaoJDBC implements SellerDao {
 			st.setInt(1, id);			
 			rs = st.executeQuery();			
 			if (rs.next()) {
-				Department department = new Department();
-				department.setId(rs.getInt("DepartmentId"));
-				department.setName(rs.getString("Name"));
-				
-				Seller seller = new Seller();
-				seller.setId(rs.getInt("Id"));
-				seller.setName(rs.getString("Name"));
-				seller.setEmail(rs.getString("Email"));
-				seller.setBaseSalary(rs.getDouble("BaseSalary"));
-				seller.setBirthDate(rs.getDate("BirthDate"));
-				seller.setDepartment(department);
+				Department department = instantiateDepartment(rs);				
+				Seller seller = instantiateSeller(rs, department);
 				
 				return seller;
 			}			
@@ -78,6 +69,27 @@ public class SellerDaoJDBC implements SellerDao {
 			DB.closeStatement(st);
 			DB.closeresultSet(rs);			
 		}
+	}
+	
+	// metodo auxiliar
+	private Department instantiateDepartment(ResultSet rs) throws SQLException {
+		Department department = new Department();
+		department.setId(rs.getInt("DepartmentId"));
+		department.setName(rs.getString("Name"));
+		
+		return department;
+	}
+
+	private Seller instantiateSeller(ResultSet rs, Department department) throws SQLException {
+		Seller seller = new Seller();
+		seller.setId(rs.getInt("Id"));
+		seller.setName(rs.getString("Name"));
+		seller.setEmail(rs.getString("Email"));
+		seller.setBaseSalary(rs.getDouble("BaseSalary"));
+		seller.setBirthDate(rs.getDate("BirthDate"));
+		seller.setDepartment(department);
+		
+		return seller;
 	}
 
 	@Override
