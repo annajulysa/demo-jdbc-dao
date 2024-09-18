f15613b81958595ecbb53ce1dacc3ae644ee93b6
Anna Almada <annasalmada@gmail.com>
2024-09-18 23:51:30 +0100

    delete implamentation

--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -72,8 +72,7 @@ public class SellerDaoJDBC implements SellerDao {
 			st = connection.prepareStatement(
 					"UPDATE seller "
 					+ "SET Name = ?, Email = ?, BirthDate = ?, BaseSalary = ?, DepartmentId = ? "
-					+ "WHERE Id = ?",
-					Statement.RETURN_GENERATED_KEYS
+					+ "WHERE Id = ?"
 					);
 			
 			st.setString(1, obj.getName());
@@ -94,8 +93,20 @@ public class SellerDaoJDBC implements SellerDao {
 
 	@Override
 	public void deleteById(Integer id) {
-		// TODO Auto-generated method stub
+		PreparedStatement st = null;
 		
+		try {
+			st = connection.prepareStatement("DELETE FROM seller WHERE Id = ?" );
+			
+			st.setInt(1, id);
+			
+			st.executeUpdate();
+			 
+		} catch (SQLException e) {
+			throw new DbException(e.getMessage());
+		} finally {
+			DB.closeStatement(st);
+		}
 	}
 
 	@Override
